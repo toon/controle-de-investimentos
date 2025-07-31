2bb4db129c99deda4302eb6de9c806b12b8de1fd
toonirc <igor.mendonca@gmail.com>
2025-07-31 12:24:48 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
-  // baseURL: "stock-api-production-f7f4.up.railway.app",
+  // baseURL: "http://192.168.0.201:3000/api",
 });
 
 export default api;
