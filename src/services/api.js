29fe3bafc8f34e1661935a80535d2540d322ab90
toon <igor.mendonca@gmail.com>
2025-08-01 10:35:08 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3000/api",
-  baseURL: "https://express-sequelize-mysql-production.up.railway.app:3000/api",
+  baseURL: "https://express-sequelize-mysql-production.up.railway.app/api",
 });
 
 export default api;
