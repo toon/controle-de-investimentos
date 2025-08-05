d7e1a25fffc2afd1797d257bd5dbb54bb4377eeb
toonirc <igor.mendonca@gmail.com>
2025-08-05 16:24:49 -0300

    sercurity for devel

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true, // ← necessário para aceitar conexões do ngrok
+  },
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
